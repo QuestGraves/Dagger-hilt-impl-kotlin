c6f0e6e7e1d7dcf381fea226e63103d92ac8e5b3
Matt Graves <QuestGraves@users.noreply.github.com>
2021-06-26 07:00:46 -0400

    Initial commit, adding base project files.

---
+++ app/src/main/java/com/keltica/dagger_hilt_impl_kotlin/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.keltica.dagger_hilt_impl_kotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
